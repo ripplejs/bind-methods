588e99b6b895a87c1a022031b4b15ea139ea56d3
Anthony Short <antshort@gmail.com>
2014-03-29 11:16:42 -0700

    Updated for 0.2.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(View) {
-  View.on('created', function(){
+  View.created(function(){
     for(var key in this) {
       if(typeof this[key] === 'function') this[key] = this[key].bind(this);
     }
